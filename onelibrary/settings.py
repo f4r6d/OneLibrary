44e8ec52a16a5fd39aa41089c7a9990536434622
farshidelf <farshidelf@yahoo.com>
2023-02-10 15:27:32 +0330

    start catalog app

--- onelibrary/settings.py
+++ onelibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+3cuwt@l1bpkawxo(t&1h-42q*_@_)t)nr&^2@x06)br0)pnh2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'onelibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
